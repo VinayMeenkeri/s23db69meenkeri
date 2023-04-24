298f3719e4301900a6672f22c136a7db0c3b6aa8
VinayMeenkeri <122691491+VinayMeenkeri@users.noreply.github.com>
2023-04-23 22:11:34 -0500

    updated

--- models/india.js
+++ models/india.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose")
 const indiaSchema = mongoose.Schema({
-    state_name: String,
-    state_population: Number,
-    state_language: String
+    state_name: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: [3, "Name of the State is not valid"],
+      },
+    state_population:  {
+        type: Number,
+        required: true,
+        min: [5000, "Population of the State is not valid"],
+      },
+    state_language: {
+        type: String,
+        required: true,
+        minLength: [2, "Language of the State is not valid"],
+      },
 })
 module.exports = mongoose.model("india",
     indiaSchema)
--- routes/india.js
+++ routes/india.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const india_controlers= require('../controllers/india');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
 
 /* GET india */
 router.get('/', india_controlers.india_view_all_Page);
@@ -9,7 +19,7 @@ router.get('/detail', india_controlers.india_view_one_Page);
 /* GET create india page */
 router.get('/create', india_controlers.india_create_Page);
 /* GET create update page */
-router.get('/update', india_controlers.india_update_Page);
+router.get('/update', secured, india_controlers.india_update_Page);
 /* GET delete india page */
 router.get('/delete', india_controlers.india_delete_Page);
 module.exports = router;
